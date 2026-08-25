a756f090a2154a7b092980cd424a708f5b6c3c1f
kamikw34 <95147720+kamikw34@users.noreply.github.com>
2026-08-25 04:19:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -54,16 +54,16 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Уверена?",
+    "Точно-точно?",
+    "А если подумать?",
+    "Юлечка, ну пожалуйста...",
+    "Ну подумай еще!",
+    "Если скажешь нет, мне будет очень грустно...",
+    "Я буду плакать...",
+    "Я уже плачу...",
+    "Хорошо, я перестану спрашивать...",
+    "Шучу, скажи ДЯ! ❤️"
 ];
 
 let messageIndex = 0;
